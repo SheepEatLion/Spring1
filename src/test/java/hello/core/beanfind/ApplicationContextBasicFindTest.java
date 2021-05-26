b7fdaa96388aa09b9d8c580ac92ab271f375c1be
SheepEatLion <gowjr207@gmail.com>
2021-05-26 21:22:03 +0900

    2021. 05. 26. 스프링 빈 조회방법들

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,12 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+
+
+}
--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -1,40 +1,50 @@
 package hello.core.beanfind;
 
 import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationContextInfoTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
-    @DisplayName("모든 빈 출력하기")
-    void findAllBean() {
-        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            Object bean = ac.getBean(beanDefinitionName);
-            System.out.println("name = "+ beanDefinitionName + " object = " + bean);
-
-        }
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        //System.out.println("memberService = " + memberService);
+        //System.out.println("memberService.getClass() = " + memberService.getClass());
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
     @Test
-    @DisplayName("애플리케이션 빈 출력하기")
-    void findApplicationBean() {
-        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    } // 역할에 의존하지 않고 구현에 의존했기 때문에 좋은 코드는 아님.
 
-            //Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
-            //Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
-            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION)
-            {
-                Object bean = ac.getBean(beanDefinitionName);
-                System.out.println("name = " + beanDefinitionName + " object = " + bean);
-            }
-
-        }
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+        // no search bean exception
+        //MemberService xxxxx = ac.getBean("xxxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class, () ->
+                ac.getBean("xxxxx", MemberService.class));
     }
 }
