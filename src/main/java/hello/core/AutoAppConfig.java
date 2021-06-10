7304e6b1eccf2fad8cdad060b1f7a57d8e29dcfb
SheepEatLion <gowjr207@gmail.com>
2021-06-10 14:58:41 +0900

    2021. 06. 10. 컴포넌트 스캔 필터 include/exclude

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // 베이스를 지정해줄 수 있고 basePackageClasses 를 사용하면 그 클래스의 패키지를 기준으로 탐색한다.
+        // 디폴트는 @ComponentScan이 붙은 클래스의 패키지로 시작하기 때문에 설정정보클래스를 프젝 최상단에 두는게 관례다.
+        basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
