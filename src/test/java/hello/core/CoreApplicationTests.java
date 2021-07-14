b6e846d580db8fc553828abad81080137619b110
SheepEatLion <gowjr207@gmail.com>
2021-07-14 17:36:27 +0900

    맥북 연동

--- src/test/java/hello/core/CoreApplicationTests.java
+++ src/test/java/hello/core/CoreApplicationTests.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class CoreApplicationTests {
 
 	@Test
+	@DisplayName("코드와 관련 없음 --> 맥북 연결 시도 후 깃허브 연동 확인용")
 	void contextLoads() {
 	}
 
